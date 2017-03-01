10e9d76d2f773cf5c4d3641e671eb641b7d21fa4
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2017-03-01 07:21:40 +0000

    Refactorinng

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
 import {
-  USERS_REQUEST, USERS_SUCCESS, USERS_FAILURE,
   FETCH_PERMISSIONS_REQUEST, FETCH_PERMISSIONS_SUCCESS, FETCH_PERMISSIONS_FAILURE,
   USER_CREATE_SUCCESS, USER_CREATE_REQUEST, USER_CREATE_FAILURE,
   GRANT_PERMISSION_REQUEST, GRANT_PERMISSION_FAILURE, GRANT_PERMISSION_SUCCESS,
@@ -21,54 +20,6 @@ import { browserHistory } from 'react-router';
 
 export const BASE_URL = 'http://0.0.0.0:8000/'
 
-
-export function requestUsers() {
-  return {
-    type: USERS_REQUEST
-  }
-}
-
-export function usersError(message) {
-  return {
-    type: USERS_FAILURE,
-    message
-  }
-}
-
-export function usersFetched(users) {
-  return {
-    type: USERS_SUCCESS,
-    users
-  }
-}
-
-export function fetchUsers() {
-  const id_token = localStorage.getItem('id_token')
-  let config = {
-    method: 'GET',
-    headers: {
-      'Authorization': `JWT ${id_token}`
-    },
-  }
-
-  // We dispatch requestLogin to kickoff the call to the API
-  return dispatch => {
-    dispatch(requestUsers())
-
-    fetch(`${BASE_URL}user/user?nest-permissions=true`, config)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json)
-        if ("error" in json) {
-          dispatch(usersError(json.message))
-        } else {
-          const { users } = json;
-          dispatch(usersFetched(users))
-        }
-      })
-      .catch(err => console.log("Error: ", err))
-  }
-}
 
 export function fetchPerimissionsRequest() {
   return {
